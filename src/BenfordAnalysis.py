f452e0fc957014304291aff713edb59e153a8142
Andrew <sternandrewm@gmail.com>
2020-11-06 23:25:15 -0600

    finish write_distribution; refactor and rename calculate_distribution to be calculate_candidate_vote_distribution; create get_candidates

--- src/BenfordAnalysis.py
+++ src/BenfordAnalysis.py
@@ -1,28 +1,49 @@
 import csv
-import ElectionResult
+# import ElectionResult
 import numpy
 # import pyplot
 
+class ElectionResult:
+    def __init__(self, data):
+        self.year = data[0]
+        self.state = data[1]
+        self.candidate = data[2]
+        self.party = data[3]
+        self.writein = data[4]
+        self.candidatevotes = data[5]
+        self.totalvotes = data[6]
+
+    def __str__(self):
+        return "year: " + self.year + "," + "state: " + self.state + "," + "candidate: " + self.candidate + "," + "candidatevotes: " + self.candidatevotes
+
 def write_distribution():
     with open(read_presidential_votes_state_data()) as csvfile:
-        unfiltered_results = csv.reader(csvfile)
-        filtered_results = filter_by_year(unfiltered_results, "1976")
-        # filtered_results = filter_by_candidate(filtered_results, "Carter, Jimmy")
-        distribution = calculate_distribution(filtered_results)
-        print(distribution)
+        raw_data = csv.reader(csvfile)
+        data_by_year = filter_by_year(raw_data, "1976")
+        candidates_by_year = get_candidates(data_by_year)
+        vote_distribution_by_candidate = dict()
+        for candidate in candidates_by_year:
+            vote_distribution_by_candidate[candidate] = calculate_candidate_vote_distribution(data_by_year, candidate)
+            print(candidate + " : ")
+            print(vote_distribution_by_candidate[candidate])
 
-def calculate_distribution(data):
+def calculate_candidate_vote_distribution(data, candidate):
     occurrences = 9 * [0]
     for range_leading_digit in range(1, 9):
         for row in data:
             if row.writein is True:
                 continue
-            vote_leading_digit = row.candidatevotes[0]
-            occurrences[int(vote_leading_digit) - 1] += 1
+            if row.candidate == candidate:
+                vote_leading_digit = row.candidatevotes[0]
+                occurrences[int(vote_leading_digit) - 1] += 1
     return occurrences
 
-def filter_by_candidate(data, candidate_name):
-    return filter_by(data, 'candidate', candidate_name)
+def get_candidates(data):
+    candidates = []
+    for row in data:
+        if row.candidate and row.candidate not in candidates:
+            candidates.append(row.candidate)
+    return candidates
 
 def filter_by_year(data, year):
     return filter_by(data, 'year', year)

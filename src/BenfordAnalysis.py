05404b286da00f9fc9fce9ef5eabf48421a91703
Andrew <sternandrewm@gmail.com>
2020-11-14 10:03:40 -0600

    working version!

--- src/BenfordAnalysis.py
+++ src/BenfordAnalysis.py
@@ -1,7 +1,4 @@
 import csv
-# import ElectionResult
-# import numpy
-# import pyplot
 import os
 from pathlib import Path
 import datetime
@@ -12,7 +9,6 @@ class ElectionResult:
         self.state = data[1]
         self.candidate = data[2].replace(",", "")
         self.party = data[3]
-        # self.writein = data[4]
         self.candidatevotes = data[4]
         self.totalvotes = data[5]
         if len(data) > 6:
@@ -33,38 +29,44 @@ def write_benford_distributions(benford_distributions):
         csvfile.close()
 
 def calculate_benford_distributions(election_results):
+    return calculate_leading_digit_proportions(sum_votes_by_leading_digit(election_results))
+
+def calculate_leading_digit_proportions(raw_vote_distribution):
+    benford_distribution = dict()
+    for year in raw_vote_distribution.keys():
+        benford_distribution[year] = dict()
+        for candidate in raw_vote_distribution[year].keys():
+            sum_of_all_leading_digit_counts = sum(raw_vote_distribution[year][candidate])
+            benford_distribution[year][candidate] = [100 * int(x) / int(sum_of_all_leading_digit_counts) for x in raw_vote_distribution[year][candidate]]
+    return benford_distribution
+
+def sum_votes_by_leading_digit(election_results):
     # basic structure of the data is nested dictionary: <year, <candidate, [benford_distribution]>>
-    benford_distributions = dict()
-
+    raw_vote_distribution = dict()
     for election_result in election_results:
-        # ignore cases where the candidate field is empty
-        if not election_result.candidate:
-            continue
+        if not raw_vote_distribution.get(election_result.year):
+            raw_vote_distribution[election_result.year] = dict()
 
-        # ignore cases where candidatevotes are missing from the county-level data
-        if election_result.candidatevotes == "NA":
-            continue
+        if not raw_vote_distribution.get(election_result.year).get(election_result.candidate):
+            raw_vote_distribution[election_result.year][election_result.candidate] = 9 * [0]
 
-        # add the year as the primary key
-        if not benford_distributions.get(election_result.year):
-            benford_distributions[election_result.year] = dict()
-
-        # add the candidate as the secondary key - initialize to empty leading digit array [0,0,...,0]
-        if not benford_distributions.get(election_result.year).get(election_result.candidate):
-            benford_distributions[election_result.year][election_result.candidate] = 9 * [0]
-
-        # calculate the benford distribution
         leading_digit = int(election_result.candidatevotes[0])
-        benford_distributions[election_result.year][election_result.candidate][leading_digit - 1] += 1
-        # benford_distributions[election_result.year][election_result.candidate] = [100 * int(x) / int(election_result.totalvotes) for x in benford_distributions[election_result.year][election_result.candidate]]
-    return benford_distributions
+        raw_vote_distribution[election_result.year][election_result.candidate][leading_digit - 1] += 1
+    return raw_vote_distribution
 
 def get_election_results(data_file_path):
     with open(data_file_path) as csvfile:
         raw_data = csv.reader(csvfile)
         results = []
         for row in raw_data:
-            results.append(ElectionResult(row))
+            election_result = ElectionResult(row)
+            if not election_result.candidate:
+                continue
+
+            if election_result.candidatevotes == "NA":
+                continue
+
+            results.append(election_result)
         csvfile.close()
         results.pop(0) #remove the header row
         return results
@@ -87,4 +89,4 @@ def get_resources_directory():
 def get_root_directory():
     return str(Path(os.path.realpath(__file__)).parent.parent) + "\\"
 
-write_benford_distributions(calculate_benford_distributions(get_election_results(read_presidential_votes_county_data())))
+write_benford_distributions(calculate_benford_distributions(get_election_results(read_presidential_votes_state_data())))

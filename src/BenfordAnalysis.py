18d32ae320981299afa43440488732f663977c14
Andrew <sternandrewm@gmail.com>
2020-11-26 10:31:12 -0600

    implemented methods for finding counties most predictive of winner and loser of elections

--- src/BenfordAnalysis.py
+++ src/BenfordAnalysis.py
@@ -57,7 +57,7 @@ def sum_votes_by_leading_digit(election_results):
 def get_election_results(data_file_path):
     with open(data_file_path) as csvfile:
         raw_data = csv.reader(csvfile)
-        results = []
+        election_results = []
         for row in raw_data:
             election_result = ElectionResult(row)
             if not election_result.candidate:
@@ -66,10 +66,10 @@ def get_election_results(data_file_path):
             if election_result.candidatevotes == "NA":
                 continue
 
-            results.append(election_result)
+            election_results.append(election_result)
         csvfile.close()
-        results.pop(0) #remove the header row
-        return results
+        election_results.pop(0) #remove the header row
+        return election_results
 
 def get_result_output_path():
     return get_results_directory() + "run-" + str(datetime.datetime.now()).replace(" ", "-").replace(":", "-") + ".csv"
@@ -89,4 +89,73 @@ def get_resources_directory():
 def get_root_directory():
     return str(Path(os.path.realpath(__file__)).parent.parent) + "\\"
 
-write_benford_distributions(calculate_benford_distributions(get_election_results(read_presidential_votes_state_data())))
+def find_counties_predictive_of_winner(prediction_rate_by_county):
+    return [county for county in prediction_rate_by_county if prediction_rate_by_county[county] == 1]
+
+def find_counties_predictive_of_loser(prediction_rate_by_county):
+    return [county for county in prediction_rate_by_county if prediction_rate_by_county[county] == 0]
+
+def get_prediction_rate_by_county(candidate_votes_by_year_county):
+    correct_predictions_by_county = dict()
+    for year in candidate_votes_by_year_county:
+        for county_prediction_result in candidate_votes_by_year_county[year]:
+            county_winner = None
+            for candidate_result in candidate_votes_by_year_county[year][county_prediction_result].items():
+                if county_winner is None:
+                    county_winner = candidate_result
+                if candidate_result[1] > county_winner[1]:
+                    county_winner = candidate_result
+            if correct_predictions_by_county.get(county_prediction_result) is None:
+                correct_predictions_by_county[county_prediction_result] = 0
+            if county_winner[0] in get_nationally_winning_candidates_by_year().values():
+                correct_predictions_by_county[county_prediction_result] += 1
+    number_of_elections = len(candidate_votes_by_year_county.keys())
+
+    prediction_rate_by_county = dict()
+    for county_prediction_result in correct_predictions_by_county.items():
+        prediction_rate_by_county[county_prediction_result[0]] = county_prediction_result[1] / number_of_elections
+    return prediction_rate_by_county
+
+def get_election_results_by_county(election_results):
+    # basic structure of the data is nested dictionary: <year, <county_by_state, <candidate, vote_count>>>
+    candidate_votes_by_year_county = dict()
+
+    for election_result in election_results:
+        # only care about major party candidates for now
+        if election_result.party != "republican" and election_result.party != "democrat":
+            continue
+
+        county_by_state = election_result.county + "," + election_result.state
+
+        if not candidate_votes_by_year_county.get(election_result.year):
+            candidate_votes_by_year_county[election_result.year] = dict()
+        if not candidate_votes_by_year_county.get(election_result.year).get(county_by_state):
+            candidate_votes_by_year_county[election_result.year][county_by_state] = dict()
+        if not candidate_votes_by_year_county.get(election_result.year).get(county_by_state).get(election_result.candidate):
+            candidate_votes_by_year_county[election_result.year][county_by_state][election_result.candidate] = dict()
+
+        candidate_votes_by_year_county[election_result.year][county_by_state][election_result.candidate] = election_result.candidatevotes
+
+    return candidate_votes_by_year_county
+
+def get_nationally_winning_candidates_by_year():
+    nationally_winning_candidates_by_year = dict()
+    nationally_winning_candidates_by_year["2000"] = "George W. Bush"
+    nationally_winning_candidates_by_year["2004"] = "George W. Bush"
+    nationally_winning_candidates_by_year["2008"] = "Barack Obama"
+    nationally_winning_candidates_by_year["2012"] = "Barack Obama"
+    nationally_winning_candidates_by_year["2016"] = "Donald Trump"
+    return nationally_winning_candidates_by_year
+
+def get_nationally_losing_candidates_by_year():
+    nationally_losing_candidates_by_year = dict()
+    nationally_losing_candidates_by_year["2000"] = "Al Gore"
+    nationally_losing_candidates_by_year["2004"] = "John Kerry"
+    nationally_losing_candidates_by_year["2008"] = "John McCain"
+    nationally_losing_candidates_by_year["2012"] = "Mitt Romney"
+    nationally_losing_candidates_by_year["2016"] = "Hillary Clinton"
+    return nationally_losing_candidates_by_year
+
+# write_benford_distributions(calculate_benford_distributions(get_election_results(read_presidential_votes_state_data())))
+print(find_counties_predictive_of_winner(get_prediction_rate_by_county(get_election_results_by_county(get_election_results(read_presidential_votes_county_data())))))
+# print(find_counties_predictive_of_loser(get_prediction_rate_by_county(get_election_results_by_county(get_election_results(read_presidential_votes_county_data())))))

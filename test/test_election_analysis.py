149c76d28803b885c7add667af3ede794af40978
Andrew <sternandrewm@gmail.com>
2021-05-10 21:02:33 -0500

    rename test_election_result to test_election_analysis in order to match src file name (election_analysis)

---
+++ test/test_election_analysis.py
@@ -0,0 +1,13 @@
+import unittest
+
+
+class ElectionAnalysisServiceTestCase(unittest.TestCase):
+    def setUp(self) -> None:
+        pass
+
+    def test_expected_written_file_text(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()

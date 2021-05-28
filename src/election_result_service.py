b237da29ef9fe06e92478d2210461f604d8ecd78
Andrew <sternandrewm@gmail.com>
2021-05-28 09:43:39 -0500

    guard against KeyErrors; add default_value param to initialize_locale_dict

--- src/election_result_service.py
+++ src/election_result_service.py
@@ -33,16 +33,18 @@ class ElectionResultService:
             filtered_results.append(election_result)
         return filtered_results
 
-    # TODO: need to guard against KeyError in the event that consumer supplies invalid key
     def get_nationally_winning_candidate_by_year(self, year):
-        return self.get_nationally_winning_candidates_by_year()[year]
+        if year in self.get_nationally_winning_candidates_by_year().keys():
+            return self.get_nationally_winning_candidates_by_year()[year]
+        return "Unknown"
 
     def get_nationally_winning_candidates_by_year(self):
         return self.election_result_repository.find_nationally_winning_candidates_by_year()
 
-    # TODO: need to guard against KeyError in the event that consumer supplies invalid key
     def get_nationally_losing_candidate_by_year(self, year):
-        return self.get_nationally_losing_candidates_by_year()[year]
+        if year in self.get_nationally_losing_candidates_by_year().keys():
+            return self.get_nationally_losing_candidates_by_year()[year]
+        return "Unknown"
 
     def get_nationally_losing_candidates_by_year(self):
         return self.election_result_repository.find_nationally_losing_candidates_by_year()
--- src/prediction_analysis_service.py
+++ src/prediction_analysis_service.py
@@ -25,17 +25,17 @@ class PredictionAnalysisService:
         return {locale: correct_predictions / number_of_elections for locale, correct_predictions in correct_predictions.items()}
 
     def sum_correct_predictions_by_locale(self, election_results):
-        locale_predictions = self.initialize_locale_dict(election_results)
+        locale_predictions = self.initialize_locale_dict(election_results, 0)
         for election_result in election_results:
             if self.was_prediction_correct(election_result):
                 locale_predictions[election_result.locale] += 1
         return locale_predictions
 
-    def initialize_locale_dict(self, election_results):
+    def initialize_locale_dict(self, election_results, default_value):
         locale_dict = dict()
         for election_result in election_results:
             if locale_dict.get(election_result.locale) is None:
-                locale_dict[election_result.locale] = 0
+                locale_dict[election_result.locale] = default_value
         return locale_dict
 
     def was_prediction_correct(self, election_result):

0426531161ccc6b1d56b88160c0a478a0b424854
Andrew <sternandrewm@gmail.com>
2021-07-03 09:45:40 -0500

    create ElectionHistoryService.get_winner_history_for_locality method

--- src/services/election_history_service.py
+++ src/services/election_history_service.py
@@ -8,21 +8,18 @@ class ElectionHistoryService:
         self.election_event_service = election_event_service
 
     def get_winning_party_history_for_locality(self, locality):
-        winning_party_history = []
-        county = locality.split(',')[0]
-        state = locality.split(',')[1]
-        for year in self.election_event_service.get_election_years():
-            winning_party = self.election_result_service.get_winning_party_for_election(year, county, state)
-            if winning_party != None:
-                winning_party_history.append(winning_party)
-        return winning_party_history
+        return self.get_winner_history_for_locality(locality, self.election_result_service.get_winning_party_for_election)
 
     def get_winning_candidate_history_for_locality(self, locality):
-        winning_candidate_history = []
+        return self.get_winner_history_for_locality(locality, self.election_result_service.get_winning_candidate_for_election)
+
+    def get_winner_history_for_locality(self, locality, callback):
+        years = self.election_event_service.get_election_years()
+        winning_candidate_history = dict.fromkeys(years, "")
         county = locality.split(',')[0]
         state = locality.split(',')[1]
-        for year in self.election_event_service.get_election_years():
-            winning_candidate = self.election_result_service.get_winning_candidate_for_election(year, county, state)
+        for year in years:
+            winning_candidate = callback(year, county, state)
             if winning_candidate != None:
-                winning_candidate_history.append(winning_candidate)
+                winning_candidate_history[year] = winning_candidate
         return winning_candidate_history
--- tests/services/test_election_history_service.py
+++ tests/services/test_election_history_service.py
@@ -1,6 +1,7 @@
 import unittest
 
 from src.repositories.mock_election_result_repository import MockElectionResultRepository
+from src.services.election_event_service import ElectionEventService
 from src.services.election_history_service import ElectionHistoryService
 from src.services.election_result_service import ElectionResultService
 
@@ -9,30 +10,61 @@ class ElectionHistoryServiceTestCases(unittest.TestCase):
     def setUp(self) -> None:
         self.mock_election_repository = MockElectionResultRepository()
         self.election_result_service = ElectionResultService(self.mock_election_repository)
-        self.election_history_service = ElectionHistoryService(self.election_result_service)
+        self.election_event_service = ElectionEventService(self.election_result_service)
+        self.election_history_service = ElectionHistoryService(self.election_event_service, self.election_result_service)
 
     def test_get_winning_party_history_for_locality(self):
-        expected = ["Republican", "Democrat"]
+        expected = {
+            "1993": "Republican",
+            "1997": "",
+            "2001": "Democrat",
+            "2005": "",
+        }
         actual = self.election_history_service.get_winning_party_history_for_locality("County 1,MO")
         self.assertEqual(expected, actual)
 
-        expected = ["Democrat", "Republican"]
+        expected = {
+            "1993": "Democrat",
+            "1997": "",
+            "2001": "Republican",
+            "2005": "",
+        }
         actual = self.election_history_service.get_winning_party_history_for_locality("County 2,MO")
         self.assertEqual(expected, actual)
 
-        expected = ["Republican", "Republican"]
+        expected = {
+            "1993": "Republican",
+            "1997": "",
+            "2001": "Republican",
+            "2005": "",
+        }
         actual = self.election_history_service.get_winning_party_history_for_locality("County 3,MO")
         self.assertEqual(expected, actual)
 
     def test_get_winning_candidate_history_for_locality(self):
-        expected = ["Fake Candidate 2", "Fake Candidate 1"]
+        expected = {
+            "1993": "Fake Candidate 2",
+            "1997": "",
+            "2001": "Fake Candidate 1",
+            "2005": "",
+        }
         actual = self.election_history_service.get_winning_candidate_history_for_locality("County 1,MO")
         self.assertEqual(expected, actual)
 
-        expected = ["Fake Candidate 1", "Fake Candidate 2"]
+        expected = {
+            "1993": "Fake Candidate 1",
+            "1997": "",
+            "2001": "Fake Candidate 2",
+            "2005": "",
+        }
         actual = self.election_history_service.get_winning_candidate_history_for_locality("County 2,MO")
         self.assertEqual(expected, actual)
 
-        expected = ["Fake Candidate 3", "Fake Candidate 2"]
+        expected = {
+            "1993": "Fake Candidate 3",
+            "1997": "",
+            "2001": "Fake Candidate 2",
+            "2005": "",
+        }
         actual = self.election_history_service.get_winning_candidate_history_for_locality("County 3,MO")
         self.assertEqual(expected, actual)

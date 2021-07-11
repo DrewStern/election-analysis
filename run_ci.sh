93f147068f818dca9e384441b710bc83cf85d7b4
Andrew <sternandrewm@gmail.com>
2021-07-11 05:38:00 -0500

    file renames

--- run_ci.sh
+++ run_ci.sh
@@ -4,11 +4,7 @@ python_version=${1:-3.9.4}
 pyenv_version=${2:-2.64.8}
 
 python -m pip install --upgrade pip
-
 ./build/get_latest_poetry.sh
-
 ./build/get_latest_pyenv.sh ${pyenv_version}
-
-./build/pyenv_local_setup.sh ${python_version}
-
-./build/coverage_install_and_run.sh
+./build/setup_pyenv_local.sh ${python_version}
+./build/run_coverage_report.sh
